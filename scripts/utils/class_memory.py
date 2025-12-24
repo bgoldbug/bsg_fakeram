2bf0e5249883f1ae32302f9657c31cc4ae0e8dbc
Benjamin Goldblatt <bgoldbla@ucsc.edu>
2025-12-24 15:45:24 -0800

    Added a feature to retry cacti macro generation if width in bits is an obscure value (tries within a range of 2 bytes and re-calculates width using original bit width / modified bit width)

--- scripts/utils/class_memory.py
+++ scripts/utils/class_memory.py
@@ -38,7 +38,7 @@ class Memory:
     # Options: 'write_first' (write-through), 'read_first' (no-change), 'write_through' (combinational)
     self.write_mode    = str(sram_data.get('write_mode', 'write_first'))
 
-    # clk_ct array contains the amount of clks for rw, r, and w ports respectively
+
     self.width_in_bytes = math.ceil(self.width_in_bits / 8.0)
     self.total_size     = self.width_in_bytes * self.depth
     if output_dir: # Output dir was set by command line option
@@ -48,7 +48,9 @@ class Memory:
       self.results_dir = os.sep.join([os.getcwd(), 'results', self.name])
     if not os.path.exists( self.results_dir ):
       os.makedirs( self.results_dir )
-  
+    
+    print(f'***************************Run for {self.name}***************************')
+
     if (process.tech_nm == 7):
       self.tech_node_nm                = 7 
       self.associativity               = 1 
@@ -59,24 +61,54 @@ class Memory:
       self.height_um, self.width_um    = get_macro_dimensions(process, sram_data)
       self.pin_dynamic_power_mW = 0.0013449
     else: 
+      if output_dir: # Output dir was set by command line option
+        p = str(Path(output_dir).expanduser().resolve(strict=False))
+        self.results_dir = os.sep.join([p, self.name])
+      else:
+        self.results_dir = os.sep.join([os.getcwd(), 'results', self.name])
+      if not os.path.exists( self.results_dir ):
+        os.makedirs( self.results_dir )
       if cacti_dir:
         self.cacti_dir = cacti_dir
       else:
         self.cacti_dir = os.environ['CACTI_BUILD_DIR']
-      self.__run_cacti()
-      with open( os.sep.join([self.results_dir, 'cacti.cfg.out']), 'r' ) as fid:
-        lines = [line for line in fid]
-      cacti_data = lines[-1].split(',')
-      self.tech_node_nm                = int(cacti_data[0])
-      self.capacity_bytes              = int(cacti_data[1])
-      self.associativity               = int(cacti_data[2])
-      self.access_time_ns              = float(cacti_data[4])
-      self.cycle_time_ns               = float(cacti_data[5])
-      self.pin_dynamic_power_mW        = float(cacti_data[8])
-      self.standby_leakage_per_bank_mW = float(cacti_data[9])
-      self.fo4_ps                      = float(cacti_data[11])
-      self.width_um                    = float(cacti_data[12])
-      self.height_um                   = float(cacti_data[13])
+      # IF size is unavailable for tag org (cacti error), try to generate as close to the same config that is configurable
+      # if no size within reason is available, terminate as usual
+      original_width_in_bits = self.width_in_bits
+      original_width_in_bytes = self.width_in_bytes
+      while True:
+        try:
+          self.__run_cacti()
+          with open( os.sep.join([self.results_dir, 'cacti.cfg.out']), 'r' ) as fid:
+            lines = [line for line in fid]
+            cacti_data = lines[-1].split(',')
+          self.tech_node_nm                = int(cacti_data[0])
+          self.associativity               = int(cacti_data[2])
+          self.access_time_ns              = float(cacti_data[4])
+          self.cycle_time_ns               = float(cacti_data[5])
+          self.pin_dynamic_power_mW        = float(cacti_data[8])
+          self.standby_leakage_per_bank_mW = float(cacti_data[9])
+          self.fo4_ps                      = float(cacti_data[11])
+          self.width_um                    = float(cacti_data[12])
+          self.height_um                   = float(cacti_data[13])
+        except FileNotFoundError:
+          print(f"Byte width of {self.width_in_bytes} doesn't work with cacti. Attempting again with different values.")
+          self.width_in_bits = self.width_in_bits + 8
+          self.width_in_bytes = math.ceil(self.width_in_bits / 8.0)
+          self.total_size     = self.width_in_bytes * self.depth
+          # Raise the same error, as the config doesn't work even within reason
+          if self.width_in_bits >= original_width_in_bits + (8 * 2):
+            raise
+        else:
+          break
+      # Revert the width values back for correct pin count (also try to size width of physical macro according to OG size)
+      if self.width_in_bits != original_width_in_bits:
+        self.width_um = self.width_um * original_width_in_bits / self.width_in_bits
+        print("*********************INFO******************************\n\n\n")
+        print(f"Re-calculating width using ratio of original width in bits ({original_width_in_bits}) to the width in bits fed to cacti ({self.width_in_bits})")
+        self.width_in_bits = original_width_in_bits
+        self.width_in_bytes = original_width_in_bytes
+
       
     
     self.cap_input_pf = 0.005
